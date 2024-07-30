54de49975065ea4cf99e746788803d386271dc80
GKavvadias <il_gato_nero@hotmail.com>
2024-07-30 11:33:05 +0300

    Adding sign out button

--- app/_components/ReservationForm.js
+++ app/_components/ReservationForm.js
@@ -11,15 +11,15 @@ function ReservationForm({ cabin, user }) {
       <div className="bg-primary-800 text-primary-300 px-16 py-2 flex justify-between items-center">
         <p>Logged in as</p>
 
-        <div className='flex gap-4 items-center'>
+        <div className="flex gap-4 items-center">
           <img
             // Important to display google profile images
-            referrerPolicy='no-referrer'
-            className='h-8 rounded-full'
+            referrerPolicy="no-referrer"
+            className="h-8 rounded-full"
             src={user.image}
             alt={user.name}
           />
-          <p>{user.name}</p>
+          <p>{user.name.split(" ").at(0)}</p>
         </div>
       </div>
 
